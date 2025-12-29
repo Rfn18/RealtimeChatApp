295f3d3017af3d7833a08bc3950caeb1f97e03b1
Rfn18 <rinofaster89@gmail.com>
2025-12-29 19:09:25 +0700

    updated commit

--- app/chat/[userId]/page.tsx
+++ app/chat/[userId]/page.tsx
@@ -1,32 +1,48 @@
 "use client";
 
+import { useAuth } from "@/app/contexts/auth-context";
 import { UserProfile } from "@/app/profile/page";
 import { getUserMatches } from "@/lib/actions/matches";
 import { useParams } from "next/navigation";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 export default function ChatConversationPage() {
-  const params = useParams;
   const [otherUser, setOtherUser] = useState<UserProfile | null>(null);
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+  const params = useParams();
+  const user = useAuth();
 
   const userId = params.userId as string;
-  const anjay = "cok";
 
   useEffect(() => {
-    async function loadMatches() {
+    async function loadUserData() {
       try {
         const userMatches = await getUserMatches();
         const matchedUser = userMatches.find((match) => match.id === userId);
-        setOtherUser(matchedUser);
+
+        if (matchedUser) {
+          setOtherUser(matchedUser);
+        } else {
+          router.push("/chat");
+        }
+
+        console.log(userMatches);
       } catch (error) {
         console.error(error);
+        router.push("/chat");
       } finally {
         setLoading(false);
       }
     }
 
-    loadMatches();
-  }, []);
+    if (user) {
+      loadUserData();
+    }
+
+    loadUserData();
+  }, [userId, router, user]);
 
   return <div></div>;
 }
